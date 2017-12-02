d42d2be8c548302f508371fb912564684e2761c2
Andy Wang <Andy.HF.Wang@gmail.com>
2017-12-02 15:03:40 -0500

    FileReader testing

--- flashcardJS.js
+++ flashcardJS.js
@@ -1,7 +1,28 @@
 var cardState = "question"
+var questionArray = [];
 
 window.onload = function(){
 	document.getElementById("cardAnswer").style.visibility = "hidden";
+}
+
+var openFile = function(event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function(){
+      var text = reader.result;
+      console.log(reader.result.substring(0, 200));
+	  questionArray = reader.result.split("\n");
+	  //document.getElementById("test").innerHTML += reader.result.substring(0, 200);
+    };
+    reader.readAsText(input.files[0]);
+	
+  };
+  
+function showArray() {
+	for (var i = 0; i < questionArray.length; i++){
+		document.getElementById("test").innerHTML += questionArray[i] + "\n";
+	}
 }
 
 function flipCard(){
