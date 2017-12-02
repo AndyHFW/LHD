d2b9ccb2dd780aab462a0f8d73ea5daa22bd968e
Andy Wang <Andy.HF.Wang@gmail.com>
2017-12-02 13:35:15 -0500

    adding onload()

---
+++ flashcardJS.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+	document.getElementById("cardText").innerHTML+="test";
+}
+
+function addText(){
+	var card = document.getElementById("cardText");
+	textarea.innerHTML += "test";
+}
