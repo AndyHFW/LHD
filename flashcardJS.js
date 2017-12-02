6fd911e014c1540fd7b04bed4673cfa487512406
Andy Wang <Andy.HF.Wang@gmail.com>
2017-12-02 13:59:00 -0500

    Flip button css change

--- flashcardJS.js
+++ flashcardJS.js
@@ -1,8 +1,19 @@
+var cardState = "question"
+
 window.onload = function(){
-	document.getElementById("cardText").innerHTML+="test";
+	document.getElementById("cardAnswer").style.visibility = "hidden";
 }
 
-function addText(){
-	var card = document.getElementById("cardText");
-	card.innerHTML = "test";
+function flipCard(){
+	/*var card = document.getElementById("cardText");
+	card.innerHTML = "test";*/
+	if (cardState == "question") {
+		document.getElementById("card").style.visibility = "hidden";
+		document.getElementById("cardAnswer").style.visibility = "visible";
+		cardState = "answer";
+	} else {
+		document.getElementById("card").style.visibility = "visible";
+		document.getElementById("cardAnswer").style.visibility = "hidden";
+		cardState = "question";
+	}
 }
